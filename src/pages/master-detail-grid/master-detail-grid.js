599917456e40ecd48156a1301a01db95884a77e9
vuhuuvan.kc <vanvh@quangich.com>
2023-12-07 11:22:30 +0700

    update

--- src/pages/master-detail-grid/master-detail-grid.js
+++ src/pages/master-detail-grid/master-detail-grid.js
@@ -12,7 +12,6 @@ import {
   Grouping,
   Paging,
   Lookup,
-  MasterDetail,
   Summary,
   RangeRule,
   RequiredRule,
@@ -26,14 +25,14 @@ import {
 } from 'devextreme-react/data-grid';
 
 import { createStore } from 'devextreme-aspnet-data-nojquery';
-import DetailGrid from './detail-grid';
 
 // Export to excel library
+import { exportDataGrid as exportDataGridToExcel } from 'devextreme/excel_exporter';
 import { Workbook } from 'exceljs';
 import { saveAs } from 'file-saver';
-import { exportDataGrid } from 'devextreme/excel_exporter';
 
 // Export to pdf library
+import { exportDataGrid as exportDataGridToPdf } from 'devextreme/pdf_exporter';
 import { jsPDF } from "jspdf";
 // Default export is a4 paper, portrait, using millimeters for units
 
@@ -66,37 +65,75 @@ const shippersData = createStore({
   },
 });
 
-// Export to Excel
-function onExportingExcel(e) {
-  const workbook = new Workbook();
-  const worksheet = workbook.addWorksheet('Main sheet');
 
-  exportDataGrid({
-    component: e.component,
-    worksheet,
-    autoFilterEnabled: true,
-  }).then(() => {
-    workbook.xlsx.writeBuffer().then((buffer) => {
-      saveAs(new Blob([buffer], { type: 'application/octet-stream' }), 'DataGrid.xlsx');
+// let exportFormats = [];
+// const onExporting = ((e) => {
+//   e.cancel = true;
+
+//   switch (e.format) {
+//     case "xlsx":
+//       const workbook = new Workbook();
+//       const worksheet = workbook.addWorksheet("Companies");
+//       exportFormats = ['xlsx'];
+//       // exportDataGridToExcel({
+//       exportDataGrid({
+//         component: e.component,
+//         worksheet: worksheet,
+//         autoFilterEnabled: true
+//       }).then(() => {
+//         workbook.xlsx.writeBuffer().then((buffer) => {
+//           saveAs(
+//             new Blob([buffer], { type: "application/octet-stream" }),
+//             "Companies.xlsx"
+//           );
+//         });
+//       });
+//       break;
+
+//     case "pdf":
+//       const doc = new jsPDF();
+//       exportFormats = ['pdf'];
+//       // exportDataGridToPDF({
+//       exportDataGrid({
+//         jsPDFDocument: doc,
+//         component: e.component,
+//         indent: 5
+//       }).then(() => {
+//         doc.save("Companies.pdf");
+//       });
+//       break;
+
+//     default:
+//       console.error("Đã xảy ra lỗi khi xuất dữ liệu")
+//   }
+// })
+
+const exportFormats = ['xlsx', 'pdf'];
+
+function onExporting(e) {
+  if (e.format === 'xlsx') {
+    const workbook = new Workbook();
+    const worksheet = workbook.addWorksheet('Companies');
+    exportDataGridToExcel({
+      component: e.component,
+      worksheet,
+      autoFilterEnabled: true,
+    }).then(() => {
+      workbook.xlsx.writeBuffer().then((buffer) => {
+        saveAs(new Blob([buffer], { type: 'application/octet-stream' }), 'Companies.xlsx');
+      });
     });
-  });
+  }
+  else if (e.format === 'pdf') {
+    const doc = new jsPDF();
+    exportDataGridToPdf({
+      jsPDFDocument: doc,
+      component: e.component
+    }).then(() => {
+      doc.save('Companies.pdf');
+    })
+  };
 }
-
-// Export to pdf
-const exportFormats = ['pdf'];
-const doc = new jsPDF();
-
-const onExporting = ((e) => {
-  const doc = new jsPDF();
-
-  exportDataGrid({
-    jsPDFDocument: doc,
-    component: e.component,
-    indent: 5,
-  }).then(() => {
-    doc.save('Companies.pdf');
-  });
-});
 
 const MasterDetailGrid = () => {
   return (
@@ -107,20 +144,18 @@ const MasterDetailGrid = () => {
         width="100%"
         height={600}
         remoteOperations={true}
-        onExporting={onExportingExcel, onExporting}
+        onExporting={onExporting}
       >
-
-        <MasterDetail
+        {/* <MasterDetail
           enabled={true}
           component={DetailGrid}
-        />
+        /> */}
 
         <Selection mode="multiple" />
         <FilterRow visible={true} />
         <HeaderFilter visible={true} />
         <GroupPanel visible={false} />
         <Scrolling mode="virtual" />
-        <Paging defaultPageSize={10} />
         <Export enabled={true} formats={exportFormats} allowExportSelectedData={true} />
 
         <Editing
@@ -137,7 +172,7 @@ const MasterDetailGrid = () => {
           <StringLengthRule max={5} message="The field Customer must be a string with a maximum length of 5." />
         </Column>
 
-        <Column dataField="OrderDate" dataType="date" width={150}>
+        <Column dataField="OrderDate" dataType="date" width={150} allowExporting={false}>
           <RequiredRule message="The OrderDate field is required." />
         </Column>
 
@@ -176,7 +211,7 @@ const MasterDetailGrid = () => {
 
         </Summary>
         <ColumnFixing enabled={true} />
-        <Export enabled={true} allowExportSelectedData={true} />
+        <Paging defaultPageSize={10} />
       </DataGrid>
     </>
   );
